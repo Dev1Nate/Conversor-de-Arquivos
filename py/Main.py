73bc712b4f661453a29ee45f8877c6df526215b8
Davi <conserdavi@gmail.com>
2025-08-23 09:11:59 -0300

    criacao do conversor para pdf

--- py/Main.py
+++ py/Main.py
@@ -1,6 +1,25 @@
 import pypandoc as doc
+import os
 
 
-#doc.convert_file
-for i in doc.get_pandoc_formats()[1]:
-    print(i)
+#NAO DA PRA CONVERTER SLIDE PRA PDF
+# docx - pdf FEITO
+# txt - pdf FEITO
+def to_pdf(tipo): 
+    x = doc.convert_file( "processo de pdf",
+                         to="pdf",
+                         format=tipo,outputfile="teste.pdf",
+                         extra_args=[
+        "--pdf-engine=xelatex",
+        "-V", "fontsize=12pt",
+        "-V", "geometry:top=2cm,left=1.5cm,right=2cm,bottom=2cm",
+        "-H", "format/header.tex" 
+    ])
+    print(x)
+
+to_pdf("pdf")
+
+
+
+
+
