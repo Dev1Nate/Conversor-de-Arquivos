51a20e807c98deacfe367a0aadee43f436d0a6ce
Davi <conserdavi@gmail.com>
2025-08-23 20:35:03 -0300

    finalização/criação do metodo de inicialização quando chamado pelo terminal

--- py/Main.py
+++ py/Main.py
@@ -1,8 +1,8 @@
 import pypandoc as doc
-import os
-from util.extract_pdf import converter 
+from UTILS.extract_pdf import converter 
+import sys
 
-#NAO DA PRA CONVERTER SLIDE PRA PDF(mas da para fazer se estiver em um linux)
+#NAO DA PRA CONVERTER SLIDE PRA NADA(mas da para fazer se estiver em um linux)
 
 def to_pdf(tipo,path): 
     if tipo == 'pdf':
@@ -14,7 +14,7 @@ def to_pdf(tipo,path):
         "--pdf-engine=xelatex",
         "-V", "fontsize=12pt",
         "-V", "geometry:top=2cm,left=1.5cm,right=2cm,bottom=2cm",
-        "-H", "format/header.tex" 
+        "-H", "FORMATS/header.tex" 
     ])
         
     else:
@@ -25,7 +25,7 @@ def to_pdf(tipo,path):
         "--pdf-engine=xelatex",
         "-V", "fontsize=12pt",
         "-V", "geometry:top=2cm,left=1.5cm,right=2cm,bottom=2cm",
-        "-H", "format/header.tex" 
+        "-H", "FORMATS/header.tex" 
     ])
     print(x)
 
@@ -65,7 +65,7 @@ def to_docx(tipo,path):
         x = doc.convert_text( text,
                          to="docx",
                          format="markdown",outputfile="teste.docx",extra_args=[
-        "--reference-doc=format/modelo.docx"
+        "--reference-doc=FORMATS/modelo.docx"
     ]
                          )
         
@@ -73,7 +73,7 @@ def to_docx(tipo,path):
         x = doc.convert_file( path,
                          to="docx",
                          format=tipo,outputfile="teste.docx",extra_args=[
-        "--reference-doc=format/modelo.docx"
+        "--reference-doc=FORMATS/modelo.docx"
     ] 
                          )
     print(x)
@@ -92,7 +92,7 @@ def to_pptx(tipo,path):
         x = doc.convert_text( text,
                          to="pptx",
                          format="markdown",outputfile="teste.pptx", extra_args=[
-        "--reference-doc=format/format.pptx"
+        "--reference-doc=FORMATS/format.pptx"
     ]
                          )
         
@@ -100,12 +100,28 @@ def to_pptx(tipo,path):
         x = doc.convert_file( path,
                          to="pptx",
                          format=tipo,outputfile="teste.pptx", extra_args=[
-        "--reference-doc=format/format.pptx"
+        "--reference-doc=FORMATS/format.pptx"
     ]
                          )
     print(x)
 
 
-to_pptx("markdown","teste/text_example.txt" )
+#to_pptx("markdown","teste/text_example.txt" )
 #to_pptx("pdf","teste/Modules.pdf")  #fica bugado devido aos tamanhos
 #to_pptx("docx","teste/world.docx")
+
+def iniciar(x,tipo,path):
+    if x == '1': #pdf
+        to_pdf(tipo,path)
+    elif x == '2': #txt
+        to_txt(tipo,path)
+    elif x == '3': #docx
+        to_docx(tipo,path)
+    elif x == '4':  #pptx
+        to_pptx(tipo,path)
+
+
+if __name__ == "__main__":  #sys.argv[1] -> Para Saber Para Que Tipo Quer 1 = pdf | 2 = txt | 3 = docx | 4 = pptx |
+                            #sys.argv[2] --> Tipo Original Do Arquivo
+                            #sys.argv[3] ---> Caminho Do Arquivo
+    iniciar(sys.argv[1],sys.argv[2],sys.argv[3])
